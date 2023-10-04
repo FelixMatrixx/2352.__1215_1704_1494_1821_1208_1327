e8a4aa6391c2ff7f288e426900d26ea48ebfc7d6
FelixMatrixx <phuc.pv0907.github@gmail.com>
2023-10-04 22:01:13 +0700

    001 Done

--- Bai001/Bai001.cpp
+++ Bai001/Bai001.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-
+void Nhap(int [], int&);
+void Xuat(int [], int);
 int main()
 {
+	int b[500];
 	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> i;
+	Nhap(b, n);
+	Xuat(b, n);
 	return 0;
+}
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap n: ";
+	cin >> n;
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+		a[i] = rand() % (200 + 1) - 100;
+}
+void Xuat(int a[], int n)
+{
+	cout << n << endl;
+	for (int i = 0; i < n; i++)
+		cout << setw(10) << a[i];
 }
