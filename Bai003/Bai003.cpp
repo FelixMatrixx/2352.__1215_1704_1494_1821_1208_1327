e87b77f9feb5d99d96cdfbc07dd3775c9f8feb4a
dqthjnh <23521494@gm.uit.edu.vn>
2023-10-05 11:09:37 +0700

    Up lan 2

--- Bai003/Bai003.cpp
+++ Bai003/Bai003.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
-
+#include<fstream>
+#include<iomanip>
 using namespace std;
-void Nhap(int[], int&);
+void Nhap(string, int[], int&);
+void Xuat(int[], int);
 int main()
 {
+	int a[10], n;
+	Nhap("Bai003.txt", a, n);
+	Xuat(a, n);
 	return 0;
 }
-void Nhap(int a[], int& n)
+void Nhap(string filename, int a[], int& n)
 {
-	cout << "Nhap n: ";
-	cin >> n;
-	srand(time(NULL));
-	for (int i = 0; i < n; i++)
-		a[i] = rand() % (200 + 1) - 100;
+	ifstream fi(filename);
+	fi >> n;
+	for (int i = 0; i <= n - 1; i++)
+		fi >> a[i];
+}
+void Xuat(int a[], int n)
+{
+	cout << n << endl;
+	for (int i = 0; i <= n - 1; i++)
+		cout << setw(10) << a[i];
 }
