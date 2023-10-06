9a500b0ed249c357cccf0ebef07e7f637a61f542
FelixMatrixx <phuc.pv0907.github@gmail.com>
2023-10-06 22:47:03 +0700

    181 done

--- Bai181/Bai181.cpp
+++ Bai181/Bai181.cpp
@@ -3,20 +3,21 @@
 
 using namespace std;
 
-void Nhap(int[], int&);
-int ktCon(int[], int, int);
-int DemConGiam(int[], int);
+void Nhap(float[], int&);
+int ViTriDuongDau(float[], int);
+float TongCon(float[], int, int);
+int ktCon(float[], int, int);
+void DuongLonNhat(float[], int, int&, int&);
 
 int main()
 {
-	int a[500];
-	int n;
+	float a[500];
+	int n, vtd, vtc;
 	Nhap(a, n);
-	int dem = DemConGiam(a, n);
-	cout << "So mang con giam: " << dem;
+	DuongLonNhat(a, n, vtd, vtc);
 	return 0;
 }
-void Nhap(int a[], int& n)
+void Nhap(float a[], int& n)
 {
 	cout << "Nhap so phan tu n: ";
 	cin >> n;
@@ -26,24 +27,52 @@ void Nhap(int a[], int& n)
 		cin >> a[i];
 	}
 }
-int ktCon(int a[], int l, int vt)
+int ViTriDuongDau(float a[], int n)
+{
+	for (int i = 0; i <= n - 1; i++)
+		if (a[i] > 0)
+			return i;
+	return -1;
+}
+float TongCon(float a[], int l, int vt)
+{
+	float s = 0;
+	for (int i = 0; i <= l - 1; i++)
+		s += a[i + vt];
+	return s;
+}
+int ktCon(float a[], int l, int vt)
 {
 	int flag = 1;
-	for (int i = 0; i <= l - 2; i++)
-		if (a[i + vt] < a[i + vt + 1])
+	for (int i = 0; i <= l - 1; i++)
+		if (a[i + vt] <= 0)
 			flag = 0;
 	return flag;
 }
-int DemConGiam(int a[], int n)
+void DuongLonNhat(float a[], int n, int& vtd, int& vtc)
 {
-	int dem = 0;
+	int vt = ViTriDuongDau(a, n);
+	if (vt == -1)
+	{
+		vtd = vtc = vt;
+		return;
+	}
+	float smax = a[vt];
+	vtd = vtc = vt;
 	for (int l = 1; l <= n; l++)
 	{
 		for (int vt = 0; vt <= n - l; vt++)
 		{
-			if (ktCon(a, l, vt) == 1)
-				dem++;
+			if (ktCon(a, l, vt) == 1 && TongCon(a, l, vt) > smax)
+			{
+				vtd = vt;
+				vtc = vt + l - 1;
+				smax = TongCon(a, l, vt);
+			}
 		}
 	}
-	return dem;
+	cout << "Tong duong lon nhat: " << smax;
+	cout << "\nDay con co tong duong lon nhat: ";
+	for (int i = vtd; i <= vtc; i++)
+		cout << setw(8) << a[i];
 }
