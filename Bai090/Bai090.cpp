33163295b23225b9615ad723bc01ddf752ca8652
BPhucKHMT <23521208@gm.uit.edu.vn>
2023-10-06 18:38:25 +0700

    090

---
+++ Bai090/Bai090.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+void Nhap(int[], int&);
+bool Ktrahoanthien( int);
+int Hoanthiendau(int[], int);
+int main()
+{
+	int n;
+	int a[100];
+	Nhap(a, n);
+	cout << Hoanthiendau(a, n);
+	return 0;
+}
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap n ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Nhap mang a[" << i << "] ";
+		cin >> a[i];
+	}
+}
+bool Ktrahoanthien( int n)
+{
+	int s = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (n % i == 0)
+			s = s + i;
+	}
+	if (s == n)
+		return true;
+	return false;
+}
+int Hoanthiendau(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (Ktrahoanthien(a[i]))
+			return a[i];
+	}
+	return -1;
+}
