d7c0d97933a24dad53d9c2414891655b8e6b2724
FelixMatrixx <phuc.pv0907.github@gmail.com>
2023-10-06 20:49:29 +0700

    update 151

--- Bai151/Bai151.cpp
+++ Bai151/Bai151.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 void Nhap(int[], int&);
-void DaoMang(int[], int);
-void DaoChan(int[], int);
+void Hoanvi(int&, int&);
+void DuaVeDau(int[], int);
 void Xuat(int[], int);
 
 int main()
@@ -13,7 +13,7 @@ int main()
 	int a[500];
 	int n;
 	Nhap(a, n);
-	DaoChan(a, n);
+	DuaVeDau(a, n);
 	Xuat(a, n);
 	return 0;
 }
@@ -27,31 +27,19 @@ void Nhap(int a[], int& n)
 		cin >> a[i];
 	}
 }
-void DaoMang(int a[], int n)
+void Hoanvi(int& a, int& b)
 {
-	int d = 0;
-	int c = n - 1;
-	while (d < c)
-	{
-		int temp = a[d];
-		a[d] = a[c];
-		a[c] = temp;
-		d++;
-		c--;
-	}
+	int temp = a;
+	a = b;
+	b = temp;
 }
-void DaoChan(int a[], int n)
+void DuaVeDau(int a[], int n)
 {
 	int b[100];
 	int k = 0;
 	for (int i = 0; i <= n - 1; i++)
-		if (a[i] % 2 == 0)
-			b[k++] = a[i];
-	DaoMang(b, k);
-	k = 0;
-	for (int i = 0; i <= n - 1; i++)
-		if (a[i] % 2 == 0)
-			a[i] = b[k++];
+		if (a[i] % 3 == 0)
+			HoanVi(a[i], a[k++]);
 }
 void Xuat(int a[], int n)
 {
