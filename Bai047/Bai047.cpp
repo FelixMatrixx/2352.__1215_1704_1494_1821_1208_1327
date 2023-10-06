69b9bc6484448cd3d1e497ac30952cfccdc0bb43
noobpyno <mlbv2005@gmail.com>
2023-10-06 07:25:51 +0700

    .

---
+++ Bai047/Bai047.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+float TongGiaTri(float[], int, float);
+int DemGiaTri(float[], int, float);
+float TrungBinhCong(float[], int, float);
+
+int main()
+{
+	float a[500];
+	int n;
+	float x;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Nhap phan tu thu " << i + 1 << ": ";
+		cin >> a[i];
+	}
+	cout << "Nhap x: ";
+	cin >> x;
+	cout << "Trung binh cong: " << TrungBinhCong(a, n, x);
+	return 0;
+}
+float TongGiaTri(float a[], int n, float x)
+{
+	float s = 0;
+	for (int i = 0; i < n; i++)
+		if (a[i] > x)
+			s = s + a[i];
+	return s;
+}
+int DemGiaTri(float a[], int n, float x)
+{
+	int dem = 0;
+	for (int i = 0; i < n; i++)
+		if (a[i] > x)
+			dem++;
+	return dem;
+}
+float TrungBinhCong(float a[], int n, float x)
+{
+	float s = TongGiaTri(a, n, x);
+	int dem = DemGiaTri(a, n, x);
+	if (dem == 0)
+		return 0;
+	return s / dem;
+}
