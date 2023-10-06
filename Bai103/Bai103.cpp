eec10bf14072df46840e3de4e52661a81a413960
FelixMatrixx <phuc.pv0907.github@gmail.com>
2023-10-06 16:00:28 +0700

    103 done

---
+++ Bai103/Bai103.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void Nhap(int[], int&);
+int ChanDau(int[], int);
+int ChanNhoNhat(int[], int);
+int main()
+{
+	int a[500];
+	int n;
+	Nhap(a, n);
+	int lc = ChanNhoNhat(a, n);
+	if (lc == -1)
+		cout << "Mang khong co so chan.";
+	else
+		cout << "So chan nho nhat trong mang la: " << lc;
+	return 0;
+}
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\nNhap a[" << i << "]:";
+		cin >> a[i];
+	}
+}
+int ChanDau(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		if (a[i] % 2 == 0)
+			return a[i];
+	return -1;
+}
+int ChanNhoNhat(int a[], int n)
+{
+	int lc = -1;
+	for (int i = 0; i < n; i++)
+		if (a[i] % 2 == 0 && a[i] < lc)
+			lc = a[i];
+	return lc;
+}
